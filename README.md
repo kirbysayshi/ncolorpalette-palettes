1af82d19a9fc0b32d02c865b12404029949b6204
Andrew Petersen <kirbysayshi@gmail.com>
2014-08-25 22:54:07 -0400

    do it

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 ncolorpalette-palettes
 ======================
 
-Palettes (pixel datas) that power the ncolorpalette and friends
+Palettes (pixel datas) that power the ncolorpalette and friends. Currently includes 4-color Gameboy, and some palettes inspired by Dragon Ball Z and Kirby's Dreamland.
+
+Usage
+-----
+
+```js
+var palettes = require('ncolorpalettes-palettes');
+
+console.log(palettes.gameboy);
+{
+  name: 'Gameboy',
+  pixels: [
+    0, 60, 16, 255,
+    6, 103, 49, 255,
+    123, 180, 0, 255,
+    138, 196, 0, 255
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,67 @@
+exports.gameboy = {
+  name: 'Gameboy',
+  pixels: [
+    0, 60, 16, 255,
+    6, 103, 49, 255,
+    123, 180, 0, 255,
+    138, 196, 0, 255
+  ]
+}
+
+exports['special-beam-cannon-cell'] = {
+  name: 'Special Beam Cannon (Cell)',
+  pixels: [
+    0, 0, 60, 255, // deep blue
+    83, 13, 65, 255, // purple
+    157, 37, 83, 255, // magenta
+    //0, 0, 0, 255, // black,
+    252, 226, 0, 255 // yellow
+  ]
+}
+
+exports['special-beam-cannon'] = {
+  name: 'Special Beam Cannon (Raditz)',
+  pixels: [
+    58, 12, 97, 255, // deep purple
+    170, 25, 174, 255, // bright purple
+    244, 59, 175, 255, // magenta
+    254, 251, 83, 255 // yellow
+    //254, 251, 231, 255 // white
+  ]
+}
+
+exports.goku = {
+  name: 'Super Saiyan Goku',
+  pixels: [
+    27, 49, 197, 255, // blue cuffs
+    23, 102, 118, 255, // ss iris
+    213, 89, 0, 255, // orange gi
+    250, 200, 203, 255, // skin
+    233, 202, 86, 255, // ss eyebrows
+    255, 234, 255, 255 // ss hair highlights
+  ]
+}
+
+exports.kamehameha = {
+  name: "Kame Hame Ha",
+  pixels: [
+    0, 5, 39, 255, // deep dark blue
+    9, 62, 189, 255, // royal blue
+    22, 86, 178, 255, // shadow blue
+    235, 223, 240, 255, // white blue
+    235, 223, 240, 255,
+    235, 223, 240, 255
+  ]
+}
+
+// http://www.colourlovers.com/palette/1652329/Muted_Kirby
+exports['muted-kirby'] = {
+  name: 'Muted Kirby',
+  pixels: [
+    34, 42, 79, 255,
+    172, 95, 139, 255,
+    207, 122, 122, 255,
+    251, 217, 216, 255/*,
+    255, 255, 255, 255*/
+  ]
+}
